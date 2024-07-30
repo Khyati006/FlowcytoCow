63b29b6b2c151ce0459a054607d00b3f11fcab0c
Khyati Joshi <112680370+Khyati006@users.noreply.github.com>
2024-07-30 08:59:42 +0000

    added footer

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app=Flask(__name__)
 @app.route("/")
 def first_function():
     return render_template('home.html')
-
+    
+@app.route("/datasub")
+def datasub():
+    return render_template('datasub.html')
 if __name__== "__main__":
   app.run(host='0.0.0.0',debug=True)
   
